7a2b9e0547ed345be060c6cf5150445527a5f484
Aqillaprly <aqillaaprly.gmail.com>
2025-03-04 00:07:24 +0700

    done

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -11,16 +11,12 @@ class UserController extends Controller
     public function index(){
         // tambah data user dengan Eloquent Model
         $data = [
-            'username' => 'customer-1',
-            'name' => 'Pelanggan',
-            'password' => Hash::make('12345'),
-            'level_id' => 4
+            'name' => 'Pelanggan Pertama',
+            
         ];
+        UserModel::where('username', 'customer-1')->update($data); 
 
-        UserModel::insert($data); // tambahkan data ke tabel m_user
-
-        // coba akses model UserModel
-        $user = UserModel::all(); // ambil semua data dari tabel m_user
-        return view('user', ['data' => $user]);
+        $user = UserModel::all();
+        return view('user', ['data'=> $user]);
     }
 }
