6a0ed94cee2e8feba5c5f770d7fec07ee09bf33e
Aqillaprly <aqillaaprly.gmail.com>
2025-03-04 12:35:22 +0700

    2.5

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -11,25 +11,20 @@ class UserController extends Controller
     public function index()
     {
         $user = UserModel::create([
-                'username' => 'manager55',
-                'name' => 'Manager55',
+                'username' => 'manager11',
+                'name' => 'Manager11',
                 'password' => Hash::make('12345'),
-                'level_id' => 2
+                'level_id' => 2,
             ],
         );
-        $user->username = 'manager56';
-
-        $user->isDirty(); //true
-        $user->isDirty('username'); //true
-        $user->isDirty('name'); //false
-        $user->isDirty(['name','username']); //true
+        $user->username = 'manager12';
 
         $user->save();
 
-        $user->isDirty(); //false
-        $user->isClean(); //true
-        dd($user->isDirty());
-        
-        return view('user', ['data'=> $user]);
+        $user->wasChanged(); //true
+        $user->wasChanged('username'); //true
+        $user->wasChanged(['username','level_id']); //true
+        $user->wasChanged('name'); //false
+        dd($user->wasChanged(['name','username'])); //true
     }
 }
