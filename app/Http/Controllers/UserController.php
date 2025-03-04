6d1ebabbdab25f85d346841600d083f86b40498b
Aqillaprly <aqillaaprly.gmail.com>
2025-03-04 12:08:57 +0700

    isDirty isClean

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -10,15 +10,25 @@ class UserController extends Controller
 {
     public function index()
     {
-        $user = UserModel::firstOrNew(
-            [
-                'username' => 'manager33',
-                'name' => 'Manager Tigga Tiga',
+        $user = UserModel::create([
+                'username' => 'manager55',
+                'name' => 'Manager55',
                 'password' => Hash::make('12345'),
                 'level_id' => 2
             ],
         );
+        $user->username = 'manager56';
+
+        $user->isDirty(); //true
+        $user->isDirty('username'); //true
+        $user->isDirty('name'); //false
+        $user->isDirty(['name','username']); //true
+
         $user->save();
+
+        $user->isDirty(); //false
+        $user->isClean(); //true
+        dd($user->isDirty());
         
         return view('user', ['data'=> $user]);
     }
