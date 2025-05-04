910188c7d619b5ca5399097ba7ed12c0492a5fcb
Aqillaprly <aqillaaprly.gmail.com>
2025-05-04 23:45:11 +0700

    practicum 1

---
+++ routes/api.php
@@ -0,0 +1,6 @@
+<?php
+use App\Http\Controllers\Api\RegisterController;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+Route::post('/register', App\Http\Controllers\Api\RegisterController::class)->name('register');
