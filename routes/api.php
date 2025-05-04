43bbe1b3a802fad4007e7c02c35b9221819ef56f
Aqillaprly <aqillaaprly.gmail.com>
2025-05-05 01:01:01 +0700

    Praktikum 2

--- routes/api.php
+++ routes/api.php
@@ -2,5 +2,10 @@
 use App\Http\Controllers\Api\RegisterController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Api\LoginController;
 
 Route::post('/register', App\Http\Controllers\Api\RegisterController::class)->name('register');
+Route::post('/login', App\Http\Controllers\Api\LoginController::class)->name('login');
+Route::middleware('auth:api')->get('/user', function(Request $request){
+    return $request->user();
+});
